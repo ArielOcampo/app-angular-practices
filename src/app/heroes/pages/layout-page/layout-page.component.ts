42b46b2dd6e3d23b1ce4a77a21d354f27cdccbb9
Ariel Ocampo <ariel.ocampo@method.com.ar>
2024-05-24 16:45:08 -0300

    feat: create app search heroes DC comics

---
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl: './layout-page.component.html',
+  styles: ``,
+})
+export class LayoutPageComponent {
+  public sideBarItems = [
+    { label: 'Listado', icon: 'label', path: './list' },
+    { label: 'Añadir', icon: 'add', path: './new-hero' },
+    { label: 'Buscar', icon: 'search', path: './search' },
+  ];
+}
---
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { MatAutocompleteActivatedEvent } from '@angular/material/autocomplete';
+import { FormControl } from '@angular/forms';
+
+import { HeroesService } from '../../services/hero.service';
+import { Hero } from '../../interfaces/hero.interfaces';
+
+@Component({
+  selector: 'app-search-page',
+  templateUrl: './search-page.component.html',
+  styles: ``,
+})
+export class SearchPageComponent {
+  public searchInput = new FormControl('');
+  public heroes: Hero[] = [];
+  public selectedHero?: Hero;
+
+  constructor(private heroesService: HeroesService) {}
+
+  searchHero() {
+    const value: string = this.searchInput.value || '';
+
+    this.heroesService
+      .getSuggestions(value)
+      .subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  onSelectedOption(event: MatAutocompleteActivatedEvent): void {
+    if (!event?.option?.value) {
+      this.selectedHero = undefined;
+      return;
+    }
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+    this.selectedHero = hero;
+  }
+}
