541b999f8b17dc2579611b4f01f8d258e9b95639
Ariel Ocampo <ariel.ocampo@method.com.ar>
2024-05-28 17:24:48 -0300

    feat: add CRUD and guard routes

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Router } from '@angular/router';
+import { User } from '../../../auth/interfaces/user.interface';
 
 @Component({
   selector: 'app-layout-page',
@@ -11,4 +14,15 @@ export class LayoutPageComponent {
     { label: 'Añadir', icon: 'add', path: './new-hero' },
     { label: 'Buscar', icon: 'search', path: './search' },
   ];
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  get user(): User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigateByUrl('auth');
+  }
 }
